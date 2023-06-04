61799beb4ae91c0c3b30c3da433d301edef7e9fa
dominik-schmid <accounts@dominik-schmid.com>
2023-06-04 17:50:48 +0200

    Added increment of solution for day 5

--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -9,6 +9,7 @@ fn main() {
 
 fn first_challenge_solution() {
     if let Ok(lines) = read_lines("./input.txt") {
+        let mut line_counter = 0;
         let mut crates: [Vec<char>; 9] = [
             vec![],
             vec![],
@@ -23,27 +24,42 @@ fn first_challenge_solution() {
 
         for line in lines {
             if let Ok(line_content) = line {
-                for (i, c) in line_content.chars().into_iter().enumerate() {
-                    // Check if we found the first line without creates => first line with number
-                    // found.
-                    if c == '1' {
-                        println!("crates {:?}", crates);
-                        break;
+                // Hard-coded way of determining in which line we are
+                // < 8 => crates
+                // > 9 => operations
+
+                // Get initial crate configuration
+                if line_counter < 8 {
+                    for (i, c) in line_content.chars().into_iter().enumerate() {
+                        // Only get the characters at the location where characters can appear and
+                        // check if is a character (!= whitespace).
+                        if [1, 5, 9, 13, 17, 21, 25, 29, 33].contains(&i) && !c.is_whitespace() {
+                            crates[(i - 1) / 4].push(c);
+                        }
                     }
 
-                    // Only get the characters at the location where characters can appear and
-                    // check if is a character (!= whitespace).
-                    if [1, 5, 9, 13, 17, 21, 25, 29, 33].contains(&i) && !c.is_whitespace() {
-                        crates[(i - 1) / 4].push(c);
-                    }
-                }
-
-                if line_content == "" {
-                    println!("Blank line found!");
-                    break;
+                // Get operations
+                } else if line_counter > 9 {
+                    // Get the numbers and filter out the operations
+                    let operations: Vec<u8> = line_content
+                        .split(' ')
+                        .enumerate()
+                        .filter_map(|(i, n)| {
+                            // Filter out the operations, i.e. every element with an even index
+                            if i % 2 == 0 {
+                                None
+                            } else {
+                                Some(n.parse::<u8>().unwrap())
+                            }
+                        })
+                        .collect();
+                    println!("{:?}", operations);
                 }
             }
+
+            line_counter += 1;
         }
+        println!("crates: {:?}", crates);
     }
 }
 
